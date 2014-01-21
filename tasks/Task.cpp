b97f0af78e7113bafcc54dd0b6f3dc0272049a9e
Raul Dominguez <Raul.Dominguez@dfki.de>
2014-01-21 16:30:38 +0100

    Increase even more the timeout the exception might be happening beacuse of current project's hardware issues.

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -100,7 +100,7 @@ void Task::updateHook()
 
     //read sample from hardware
     base::samples::LaserScan reading;
-    int timeout = 10 * m_devicePeriod * 1000;
+    int timeout = 10 * m_devicePeriod * 100000;
     bool first_time = true;
     while (first_time || m_driver->hasPacket())
     {
